3d48a1b52418ac251d42e614a626beed12ad35a7
TurboZ0 <87539784+TurboZ0@users.noreply.github.com>
2021-12-06 13:14:46 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,32 @@
+z=[];
+status1="";
+function preload() {
+    load_image= loadImage("Bedroom.jpg")
+}
+
+function setup() {
+    canvas=createCanvas(600,500);
+    canvas.position(700,300);
+
+    loading_model= ml5.objectDetector("cocossd", modelHasLoaded);
+
+}
+
+function modelHasLoaded() {
+    console.log("Cocossd is a buiscuit's name");
+    status1=true;
+    loading_model.detect(hold_image, recieve_result);
+}
+function recieve_result(error,result) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+      console.log(result);
+        z=result;
+    }
+    }
+
+function draw() {
+    image(load_image, 0, 0, 600, 600);
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom() {
+    window.location="bedroom.html";
+}
+function television() {
+    window.location="tv.html";
+}
+function computer() {
+    window.location="computer.html";
+}
+function fan() {
+    window.location="fan.html";
+}
+function bottle() {
+    window.location="bottle.html";
+}
